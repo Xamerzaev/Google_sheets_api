fd994cecb67a7fb6512de8e2aabbffa1eaaac229
Mansur <mansur.ham@mail.ru>
2022-06-22 01:51:47 +0300

    requirements freezed

--- core/routes.py
+++ core/routes.py
@@ -1,8 +1,8 @@
+from flask import render_template, request
+import datetime
 from core import app
-
 from core.models import db, Table
 from core.convert import usd_rate
-from flask import render_template, request
 
 @app.route('/')
 def index(dollar):
@@ -11,3 +11,7 @@ def index(dollar):
     page = request.args.get('page', 1, type=int)
     posts = Table.query.paginate(page=page, per_page=50)
     return render_template('index.html', posts=posts, ruble=ruble)
+
+def telegram(supply):
+    if datetime.datetime.now() > Table.query.get(supply):
+        
